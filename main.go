d9b582aae6b6535a42e657ece22896f4f51b3a61
jeinostroza <jeinostroza@hotmail.com>
2019-11-07 09:08:45 -0600

    add numcontainer func

--- main.go
+++ main.go
@@ -5,18 +5,24 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("client")))
 	http.HandleFunc("/login", login)
-	//http.HandleFunc("/numcontainer", numcontainer)
+	http.HandleFunc("/numcontainer", numcontainer)
 	http.ListenAndServe(":9000", nil)
 }
 
 //Loggedin is a structure whic will hold the value to let the user into the server to edit information
 type Loggedin struct {
 	Signedin bool
+}
+
+//Numcont is a structure for number of containers
+type Numcont struct {
+	Numcon bool
 }
 
 //login function should verify entered usernamen and password against the database data
@@ -43,5 +49,15 @@ func login(response http.ResponseWriter, request *http.Request) {
 //should be a nice display page welcoming the user into webserver asking how many
 //alpine images they would like to run
 func numcontainer(response http.ResponseWriter, request *http.Request) {
+	numcon := Numcont{false}
+	numcust := request.FormValue("numcontainer")
+	temp1, _ := template.ParseFiles("client/templates/numcontainer.html")
+	numcust1, _ := strconv.Atoi(numcust)
+	if numcust1 > 0 {
+		numcon.Numcon = true
+	} else {
+		numcon.Numcon = false
+	}
 
+	fmt.Println(temp1.Execute(response, numcon))
 }
