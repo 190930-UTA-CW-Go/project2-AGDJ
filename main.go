d8e136855e4a86fcf6d55ba8c7d6342027e98be0
David Chang <davidychang@outlook.com>
2019-11-07 09:58:47 -0600

    added functions

--- main.go
+++ main.go
@@ -4,26 +4,13 @@ import (
 	"fmt"
 
 	"github.com/190930-UTA-CW-Go/project2-AGDJ/ssh"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-	login, password, ip := login()
-
 	fmt.Println(ssh.CmdGetInfo(login, ip))
-	ssh.SetupDocker(login, password, ip)
-}
-
-func login() (login string, password string, ip string) {
-	fmt.Print("Login: ")
-	fmt.Scan(&login)
-	fmt.Print("Password: ")
-	result, _ := terminal.ReadPassword(0)
-	password = string(result)
-	fmt.Println()
-	fmt.Print("IP Address: ")
-	fmt.Scan(&ip)
-	fmt.Println()
-
-	return
+	//ssh.SetupDocker(login, password, ip)
+	fmt.Println(ssh.DockerStatus(login, ip))
+	fmt.Println(ssh.ListContainers(login, password, ip))
+	fmt.Println(ssh.ListImages(login, password, ip))
+	//fmt.Println(ssh.TestRun(login, password, ip))
 }
